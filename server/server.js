b34946eeadfa49345525bda11fdb937389830e5a
bruno Ezemba <brunoaugustine2@gmail.com>
2019-07-09 11:50:14 +0100

    create route folder

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
+import v1Router  from './routes'
+
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/api/v1', v1Router)
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Welcome to way_ferer API'})
